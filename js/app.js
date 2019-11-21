cc1970a6f201399238c19b3bbbf0bf696a709295
captainAldi <renaldiyulvianda@yahoo.come>
2019-11-21 14:02:00 +0700

    buat log yg bagus

--- js/app.js
+++ js/app.js
@@ -3,16 +3,23 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns: []
   },
   methods: {
     startGame: function () {
       this.gameIsRunning = true
       this.playerHealth = 100
       this.monsterHealth = 100
+      this.turns=[]
     },
     attack: function () {
-      this.monsterHealth -= this.calculateDamage(3, 10)
+      var damage = this.calculateDamage(3, 10)
+      this.monsterHealth -= damage
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player menyerang dengan ' + damage
+      })
       if(this.checkWin()) {
         return
       }
@@ -20,11 +27,21 @@ new Vue({
       this.monsterAttack()
     },
     monsterAttack: function() {
-      this.playerHealth -= this.calculateDamage(5,12)
+      var damage = this.calculateDamage(5, 12)
+      this.playerHealth -= damage
       this.checkWin()
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster menyerang dengan ' + damage
+      })
     },
     specialAttack: function () {
-      this.monsterHealth -= this.calculateDamage(12, 10)
+      var damage = this.calculateDamage(12, 10)
+      this.monsterHealth -= damage
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player menyerang dengan Speacial Attack' + damage
+      })
       if(this.checkWin()) {
         return
       }
@@ -37,6 +54,10 @@ new Vue({
       } else {
         this.playerHealth = 100
       }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heal dengan 10'
+      })
       this.monsterAttack()
     },
     giveUp: function () {
